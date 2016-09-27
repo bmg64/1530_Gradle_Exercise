9e5ab89757c25d3e95b43f1f9750adcd79783eab
Benjamin Graham <bmg64@pitt.edu>
2016-09-27 14:07:41 -0400

    sa

--- src/main/java/GradleExercise.java
+++ src/main/java/GradleExercise.java
@@ -1,6 +1,9 @@
 public class GradleExercise {
 
 	public static void main(String[] args){
+		for(int i = 0; i < args.length; i++){
+			System.out.println(i + ", " + args[i]);
+		}
 		try
 		{
 			Integer.parseInt(args[2]);
