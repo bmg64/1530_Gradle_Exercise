7bd67e5b578347280bf2083b7d6f94805536b6e6
Benjamin Graham <bmg64@pitt.edu>
2016-09-27 13:51:48 -0400

    s

--- src/main/java/GradleExercise.java
+++ src/main/java/GradleExercise.java
@@ -1,10 +1,15 @@
 public class GradleExercise {
 
 	public static void main(String[] args){
-		if(args[2] != (int)args[2]){
-			System.out.println("Second argument must be a 32-bit int.");
-			System.exit(2);		
+		try
+		{
+			Integer.parseInt(args[2]);
 		}
+		catch (NumberFormatException ex)
+		{
+			System.out.println("Second argument must be a 32-bit int.");
+			System.exit(2);
+		}		
 		if(args[1].equals("triangle")){
 			System.out.println(triangle(Integer.parseInt(args[2])));
 		}
