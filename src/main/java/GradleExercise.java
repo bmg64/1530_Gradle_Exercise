23acf1c340b5210f2c82950ede70466a79bca962
Benjamin Graham <bmg64@pitt.edu>
2016-09-27 14:08:49 -0400

    d

--- src/main/java/GradleExercise.java
+++ src/main/java/GradleExercise.java
@@ -1,23 +1,20 @@
 public class GradleExercise {
 
 	public static void main(String[] args){
-		for(int i = 0; i < args.length; i++){
-			System.out.println(i + ", " + args[i]);
-		}
 		try
 		{
-			Integer.parseInt(args[2]);
+			Integer.parseInt(args[1]);
 		}
 		catch (NumberFormatException ex)
 		{
 			System.out.println("Second argument must be a 32-bit int.");
 			System.exit(2);
 		}		
-		if(args[1].equals("triangle")){
-			System.out.println(triangle(Integer.parseInt(args[2])));
+		if(args[0].equals("triangle")){
+			System.out.println(triangle(Integer.parseInt(args[1])));
 		}
-		else if(args[1].equals("lazy")){
-			System.out.println(lazy(Integer.parseInt(args[2])));			
+		else if(args[0].equals("lazy")){
+			System.out.println(lazy(Integer.parseInt(args[1])));			
 		}
 		else{
 			System.out.println("First argument must be \"lazy\" or\"triangle\".");
